8430441742c9cf7ddbb6145196d89fb8444ad546
natukin1978 <natukin1978@hotmail.com>
2026-07-20 00:48:35 +0900

    Copy files used in other projects

---
+++ constants.py
@@ -0,0 +1,6 @@
+CALLBACK_URL_BOT = "http://localhost:4343/oauth?scopes=" + "%20".join([
+    "user:read:chat",
+    "user:bot",
+]) + "&force_verify=true"
+
+CALLBACK_URL_OWNER = "http://localhost:4343/oauth?scopes=channel:bot&force_verify=true"
